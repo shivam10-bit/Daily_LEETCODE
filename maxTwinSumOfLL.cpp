dcf0f8fb740400bcbff2c9f432203fffc9022af8
Shivam <shivamshrivatava004@gmail.com>
2026-06-14 22:18:08 +0530

    max twin sum of a linked list

---
+++ maxTwinSumOfLL.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int pairSum(ListNode* head) {
+
+        // Find middle
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        while (fast && fast->next) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+
+        // Reverse second half
+        ListNode* prev = NULL;
+        ListNode* curr = slow;
+
+        while (curr) {
+            ListNode* nextNode = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = nextNode;
+        }
+
+        // Calculate maximum twin sum
+        int maxi = 0;
+
+        ListNode* first = head;
+        ListNode* second = prev;
+
+        while (second) {
+            maxi = max(maxi, first->val + second->val);
+
+            first = first->next;
+            second = second->next;
+        }
+
+        return maxi;
+    }
+};
